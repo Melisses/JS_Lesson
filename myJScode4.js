de0712d9de885f452940af417219cc0162a3687d
Melisses <59730336+Melisses@users.noreply.github.com>
2020-02-06 14:09:11 +0200

    good version

--- myJScode4.js
+++ myJScode4.js
@@ -1,24 +1,24 @@
 const Vector = function(x, y) {
     this.x = x;
     this.y = y;
+}
 
-    this.plus = (obj) => {
-        this.x += obj.x;
-        this.y += obj.y;
-    return {x: this.x, y: this.y};
-    }
+Vector.prototype = {
+  plus: function(obj) {
+    this.x += obj.x;
+    this.y += obj.y;
+  return this;
+  },
 
-    this.minus = (obj) => {
-        this.x -= obj.x;
-        this.y -= obj.y;
-    return {x: this.x, y: this.y};
-    }
+  minus: function(obj) {
+    this.x -= obj.x;
+    this.y -= obj.y;
+  return this;
+  },
 
-    Object.defineProperty(this, "length", {
-        get: function() {
-        return Math.sqrt(Math.pow(this.x,2) + Math.pow(this.y,2));
-        }
-    })    
+  get length() {
+    return Math.sqrt(Math.pow(this.x,2) + Math.pow(this.y,2));
+  }
 }
 
 console.log(new Vector(1, 2).plus(new Vector(3, 4)));
