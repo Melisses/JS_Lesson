f1c0fa12db0155e4c1579b905572c1ca27a489d3
Melisses <59730336+Melisses@users.noreply.github.com>
2020-02-07 09:16:43 +0200

    Задача №2. Глубокое сравнение

--- myJScode2.js
+++ myJScode2.js
@@ -1,11 +1,15 @@
+//Задача 2. Глубокое сравнение. Почему-то ощущение, что должо решаться с замыканием, но пока ещё не до конца поняла его механизм
 const deepEqual = function(obj1, obj2) {
     //console.log(typeof(obj1),typeof(obj2));
-    if (obj1 === obj2) return true;
+    if (obj1 === obj2) return true; //если объекты идентичны или ссылаются на один объект
     
-    if (obj1 == null || obj2 == null || typeof(obj1) !== typeof(obj2) || obj1.length != obj2.length) return false;
-
+    //отсеиваем если значения null, не совпадают типы или сравниваемые элементы разной длины
+    if (obj1 == null || obj2 == null || typeof(obj1) !== typeof(obj2) || obj1.length != obj2.length) return false; я 
+    
+    //отсеиваем, если сравнение не прошли НЕ объекты
     if (typeof(obj1) != 'object' || typeof(obj2) != 'object') return false;
 
+    //непосредственный перебор из одного объекта и сравнение с рекурсией
     for (var key in obj1) {
         //console.log(key,obj1[key],obj2[key]);
         if ((!key in obj2) || !deepEqual(obj1[key],obj2[key])) 
